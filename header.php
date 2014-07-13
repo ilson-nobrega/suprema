6cb4f6c1543f7d19e0601bf5bc89990b23b38bb2
Wesley Sousa <wesley@viru.ga>
2014-07-13 15:53:52 -0300

    wp_head

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
         <link href="<?php echo get_stylesheet_directory_uri() ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css">
         <!-- Custom Css -->
         <link rel="stylesheet" type="text/css" media="all" href="<?php echo get_stylesheet_uri(); ?>" />
+
+        <?php wp_head(); ?>
         <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
