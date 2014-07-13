e57fe4c22b9e878a1e29dc42df1650edb43fdd5d
Wesley Sousa <wesley@viru.ga>
2014-07-13 16:43:16 -0300

    col-md na page

--- page.php
+++ page.php
@@ -2,8 +2,13 @@
 
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <article>
-            <h2><?php the_title(); ?></h2>
-            <?php the_content(); ?>
+            <div class="col-md-4">
+                <?php the_post_thumbnail('thumbnail'); ?>
+                <h2><?php the_title(); ?></h2>
+            </div>
+            <div class="col-md-6">
+                <?php the_content(); ?>
+            </div>
         </article>
     <?php endwhile?>
      
