9198bd773c142554cb61986a21f9460e17167141
Ilson Nóbrega <ilson@inobrega.com.br>
2014-07-13 12:39:51 -0300

    Alteração no Functions

---
+++ functions.php
@@ -0,0 +1,31 @@
+<?php
+
+    /**
+	 * Adiciona os estilos utilizados no template
+	 */
+		function suprema_estilos(){
+			
+			wp_enqueue_style( 'custom_style', get_template_directory_uri() . '/style.css' );
+			wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
+			
+		}
+	
+	/**
+	 * Adiciona os scripts que serão utilizados no template
+	 */
+		function suprema_scripts(){
+		    
+		    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery-2.1.1.min.js' );
+		    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js' );
+		    
+		}
+	
+	/**
+	 * Lista de todos os registros
+	 * OBS: Os registros das bibliotecas encontram-se no arquivo de cada uma
+	 */
+		// Registra os estilos personalizados
+		add_action('wp_enqueue_scripts','suprema_estilos');
+		// Registra os scripts do template
+		add_action('wp_enqueue_scripts','suprema_scripts');
+?>
--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <html>
     <head>
         <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
-        <!-- Bootstrap -->
-        <link href="<?php echo get_stylesheet_directory_uri() ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-        <!-- Custom Css -->
-        <link rel="stylesheet" type="text/css" media="all" href="<?php echo get_stylesheet_uri(); ?>" />
         <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -54,8 +50,4 @@
             </div>
         </footer>
     </body>
-    <!-- jQuery - Google Hosted Libraries -->
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Bootstrap JS -->
-    <script src="<?php echo get_stylesheet_directory_uri() ?>/js/bootstrap.min.js"></script>
 </html>
