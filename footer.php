5d3573e82ceeabf3371657b5a6e9a516a852663c
Ilson Nóbrega <ilson@inobrega.com.br>
2014-07-20 20:11:24 -0300

    Implementação do Footer

--- footer.php
+++ footer.php
@@ -2,11 +2,44 @@
             </div>
         </section>
         <footer>
-            <div class="redessociais">
-                F, T, Y
+            <div class="row-fluid rodape">
+                <div class="container widgets">
+                    <div class="col-md-4">
+                        <span class="col-md-12 span">
+                            Newsletter
+                        </span>
+                        <div class="col-md-12 footerContent">
+                            <form>
+                                <input type="text" class="form-control" placeholder="nome">
+                                <input type="text" class="form-control" placeholder="email">
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                         <span class="col-md-12 span">
+                            Contato
+                        </span>
+                        <div class="col-md-12 footerContent">
+                     
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <span class="col-md-12 span">
+                            Facebook
+                        </span>
+                        <div class="col-md-12 branco footerContent">
+                            <div class="fb-like-box facebook" data-href="https://www.facebook.com/FacebookDevelopers" data-width="380" data-colorscheme="light" data-show-faces="false" data-header="false" data-stream="false" data-show-border="true"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="copy">
-                <p>Copyright <?php echo date('Y'); ?> | Suprema Requinte</p>
+            <div class="row-fluid rodapeContainer">
+                <div class="redessociais">
+                    F, T, Y
+                </div>
+                <div class="copy">
+                    <p>Copyright <?php echo date('Y'); ?> | Suprema Requinte</p>
+                </div>
             </div>
         </footer>
     </body>
